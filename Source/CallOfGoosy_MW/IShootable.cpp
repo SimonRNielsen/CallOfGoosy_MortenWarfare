25f87c1f25134abcc92e4b3d917decda1466aaa9
SimonRNielsen <simonrn@gmail.com>
2026-04-20 13:16:48 +0200

    Added unreal-interface -> IShootable

---
+++ Source/CallOfGoosy_MW/IShootable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IShootable.h"
+
+// Add default functionality here for any IIShootable functions that are not pure virtual.
