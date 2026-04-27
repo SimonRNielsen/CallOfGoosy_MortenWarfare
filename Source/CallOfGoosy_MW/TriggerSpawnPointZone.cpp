b29055c6e775d42003098e14e73b8694ec661201
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2026-04-27 13:55:15 +0200

    Oprettet SpawnTriggerPointZone til at spawne enemy og opdateret Behavier tree

---
+++ Source/CallOfGoosy_MW/TriggerSpawnPointZone.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TriggerSpawnPointZone.h"
+
+// Sets default values
+ATriggerSpawnPointZone::ATriggerSpawnPointZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//Collisionbox
+	CollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+
+	RootComponent = CollisionComponent;
+}
+
+// Called when the game starts or when spawned
+void ATriggerSpawnPointZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	CollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ATriggerSpawnPointZone::OnOverlapBegin);
+	CollisionComponent->OnComponentEndOverlap.AddDynamic(this, &ATriggerSpawnPointZone::OnOverlapEnd);
+}
+
+// Called every frame
+void ATriggerSpawnPointZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATriggerSpawnPointZone::OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (GEngine)
+	{
+		//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Overlab Begin"));
+		HandleEnemySpawn();
+	}
+}
+
+//
+//void ATriggerSpawnPointZone::OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+//{
+//	if (GEngine)
+//	{
+//		GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, TEXT("Overlap End"));
+//	}
+//}
+
---
+++ Source/CallOfGoosy_MW/TriggerSpawnPointZone.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/Engine.h"
+#include "Components/BoxComponent.h"
+#include "TriggerSpawnPointZone.generated.h"
+
+UCLASS()
+class CALLOFGOOSY_MW_API ATriggerSpawnPointZone : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATriggerSpawnPointZone();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//Collisionbox
+	UPROPERTY(VisibleAnywhere /*BlueprintReadWrite, Category = "SpawnPoint|Collisionbox"*/);
+	UBoxComponent* CollisionComponent;
+
+	/** called when something enters the sphere component */
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	/** called when something leaves the sphere component */
+	//UFUNCTION()
+	//void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "TriggerSpawnPointZone|SpawnActor")
+	void HandleEnemySpawn();
+};
